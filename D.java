8ec44fe34676e45c0dfb91813118322fd55bc1fc
Eduardo Coelho <eduardo.coelho1@estudante.ufla.br>
2024-10-28 17:14:16 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,11 @@
+public class D {
+    private char D1;
+    private int D2;
+    public void MD1() {
+	System.out.println("MD1");
+    }
+    public void MD2() {
+	System.out.println("MD2");
+    }
+}
+
